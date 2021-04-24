c8e5e36c995c870b026021e48533e799832e6506
Karina Imani <imani.karina@gmail.com>
2021-04-24 17:56:14 +0700

    Tested breeder (- unique skill mastery lv) + fixes

--- Java/breeder.java
+++ Java/breeder.java
@@ -1,37 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
-// ADDED FUNCTIONS
-// database_engimon: findByElement
+// import java.util.List;
 
 public class breeder {
-    private String[] list_of_elements = {"Fire", "Water", "Electric", "Ground", "Ice"};
-    private double[][] element_advantage = {{1, 0, 1, 0.5, 2}, {2, 1, 0, 1, 1}, {1, 2, 1, 0, 1.5}, {1.5, 1, 2, 1, 0}, {0, 1, 0.5, 2, 1}};
-
     // Generates a random number between 0 and max - 1
     public int rng (int max) {
-        int num = (int) Math.random() * (max);
+        int num = (int) (Math.random() * (max));
         return num;
     }
 
     // Main breeder function
     public static engimon breed (String name, engimon parent1, engimon parent2) throws Exception {
-        if (parent1.getLevel() > 30 && parent2.getLevel() > 30) {
+        if (parent1.getLevel() > 3 && parent2.getLevel() > 3) {
             breeder a = new breeder();
             
             String sp = a.inheritSpecies(parent1, parent2);
             engimon child = new player_engimon(name, parent1, parent2, sp, 1, 0);
-            // !!! Engimon is abstract, wait for subclass implementation !!!
     
+            // !!! Skill bawaan kalau parent1 dan parent2 speciesnya sama? !!!
             a.inheritSkill(child, parent1, parent2);
     
-            parent1.setLevel(parent1.getLevel() - 30);
-            parent2.setLevel(parent2.getLevel() - 30);
+            parent1.setLevel(parent1.getLevel() - 3);
+            parent2.setLevel(parent2.getLevel() - 3);
     
-            System.out.println("Breeding berhasil dilakukan\n");
+            System.out.println("\nBreeding berhasil dilakukan\n");
             child.showAttributes();
             return child;
         } else {
-            throw new Exception("Error, salah satu atau kedua parent memiliki level di bawah 31");
+            throw new Exception("Error, salah satu atau kedua parent memiliki level di bawah 4");
         }
     }
 
@@ -46,20 +40,22 @@ public class breeder {
             // Get one element from each parent
             String parent_elmt1 = getParentElement(parent1);
             String parent_elmt2 = getParentElement(parent2);
+            database_engimon db = new database_engimon();
+
+            // System.out.println(parent_elmt1 + " " + parent_elmt2);
 
             if (parent_elmt1.equals(parent_elmt2)) {
-                // Case 1: both parents have same selected element, child inherits randomized species
-                return getRandom(parent1.getSpecies(), parent2.getSpecies());
+                // Case 1: both parents have same selected element, return single element child
+                return db.findByElement(parent_elmt1, null).speciesName;
             } else {
                 // Case 2: parents have different selected elements, get higher element advantage species
-                double elmt_adv = getElementAdvantage(parent_elmt1, parent_elmt2) - getElementAdvantage(parent_elmt2, parent_elmt1);
+                double elmt_adv = element_advantage.getElementAdvantage(parent_elmt1, parent_elmt2) - element_advantage.getElementAdvantage(parent_elmt2, parent_elmt1);
                 if (elmt_adv > 0) {
-                    return parent1.getSpecies();
+                    return db.findByElement(parent_elmt1, null).speciesName;
                 } else if (elmt_adv < 0) {
-                    return parent2.getSpecies();
+                    return db.findByElement(parent_elmt2, null).speciesName;
                 } else {
                     // If elements have no edvantage over the other, child inherits both
-                    database_engimon db = new database_engimon();
                     return db.findByElement(parent_elmt1, parent_elmt2).speciesName;
                 }
             }
@@ -67,10 +63,35 @@ public class breeder {
     }
 
     public void inheritSkill(engimon child, engimon parent1, engimon parent2) {
-        // Implement
         int iter1[] = {0, 0, 0, 0};
         int iter2[] = {0, 0, 0, 0};
         int max, idxmax;
+
+        // Mark unique skills
+        if (parent1.getSpecies() == child.getSpecies()) {
+            iter1[0] = 1;
+        }
+        if (parent2.getSpecies() == child.getSpecies()) {
+            iter2[0] = 1;
+        }
+
+        // Mark skills that are null
+        for (int i = 1; i < 4; i++) {
+            if (parent1.getMove(i) == null) {
+                iter1[i] = 1;
+            }
+            if (parent2.getMove(i) == null) {
+                iter2[i] = 1;
+            }
+        }
+        
+        // for (int a : iter1) {
+        //     System.out.print(a);
+        // }
+        // System.out.println();
+        // for (int a : iter2) {
+        //     System.out.print(a);
+        // }
 
         int i = 1;
         Boolean end = false; // True if no more skills can be learned
@@ -80,19 +101,21 @@ public class breeder {
             idxmax = -1;
 
             // Find skill with max mastery level
-            for (int j = 1; j < 4; j++) {
+            for (int j = 0; j < 4; j++) {
                 if (iter1[j] != 1) {
-                    if (!isLearnable(parent1.getMove(j), child)) {
-                        iter1[j] = 1;
-                    } else if (parent1.getMove(j).getMasteryLv() > max) {
+                    // if (!isLearnable(parent1.getMove(j), child)) {
+                    //     iter1[j] = 1;
+                    // }
+                    if (parent1.getMove(j).getMasteryLv() > max) {
                         max = parent1.getMove(j).getMasteryLv();
                         idxmax = j;
                     }
                 }
                 if (iter2[j] != 1) {
-                    if (!isLearnable(parent2.getMove(j), child)) {
-                        iter2[j] = 1;
-                    } else if (parent2.getMove(j).getMasteryLv() > max) {
+                    // if (!isLearnable(parent2.getMove(j), child)) {
+                    //     iter2[j] = 1;
+                    // }
+                    if (parent2.getMove(j).getMasteryLv() > max) {
                         max = parent2.getMove(j).getMasteryLv();
                         idxmax = j + 4;
                     }
@@ -114,13 +137,15 @@ public class breeder {
 
                     // Mark skills that both parents have
                     for (int j = 1; j < 4; j++) {
-                        if (chosen_skill.getSkillName() == parent2.getMove(j).getSkillName()) {
-                            iter2[j] = 1;
-                            if (chosen_skill.getMasteryLv() == parent2.getMove(j).getMasteryLv()) {
-                                // Learn skill, chosen mastery level + 1
-                                mastery = mastery + 1;
+                        if(iter2[i] != 1) {
+                            if (chosen_skill.getSkillName() == parent2.getMove(j).getSkillName()) {
+                                iter2[j] = 1;
+                                if (chosen_skill.getMasteryLv() == parent2.getMove(j).getMasteryLv() && mastery < 3) {
+                                    // Learn skill, chosen mastery level + 1
+                                    mastery = mastery + 1;
+                                }
+                                break;
                             }
-                            break;
                         }
                     }
                 // Check 2nd parent's skills
@@ -128,13 +153,14 @@ public class breeder {
                     iter2[idxmax - 4] = 1;
                     chosen_skill = parent2.getMove(idxmax - 4);
                     mastery = chosen_skill.getMasteryLv();
-                    // chosen_skill.printAll();
 
                     // Mark skills that both parents have
                     for (int j = 0; j < 4; j++) {
-                        if (chosen_skill.getSkillName() == parent1.getMove(j).getSkillName()) {
-                            iter1[j] = 1;
-                            break;
+                        if (iter1[i] != 1) {
+                            if (chosen_skill.getSkillName() == parent1.getMove(j).getSkillName()) {
+                                iter1[j] = 1;
+                                break;
+                            }
                         }
                     }
                 }
@@ -147,34 +173,23 @@ public class breeder {
     }
 
     public Boolean sameElements(engimon a, engimon b) {
-        if (a.getElmt1().equals(b.getElmt2()) && a.getElmt1().equals(b.getElmt2())) {
-            return true;
-        } else {
-            return false;
+        if (a.getElmt1().equals(b.getElmt1())) {
+            if (a.getElmt2() != null && b.getElmt2() != null) {
+                return (a.getElmt2().equals(b.getElmt2()));
+            }
+            if (a.getElmt2() == null && b.getElmt2() == null) {
+                return true;
+            }
         }
+        return false;
     }
 
     public String getParentElement(engimon x) {
-        if (x.getElmt2().equals(engimon.name_null)) {
+        if (x.getElmt2() == null) {
             return x.getElmt1();
         } else {
             return getRandom(x.getElmt1(), x.getElmt2());
         }
-    }
-
-    public double getElementAdvantage(String elmt1, String elmt2) {
-        int i = 0;
-        int idx1 = -1;
-        int idx2 = -1;
-        while (idx1 == -1 || idx2 == -1) {
-            if (list_of_elements[i].equals(elmt1)) {
-                idx1 = i;
-            } else if (list_of_elements[i].equals(elmt2)) {
-                idx2 = i;
-            }
-            i++;
-        }
-        return element_advantage[idx1][idx2];
     }
 
     public String getRandom(String a, String b) {
@@ -186,13 +201,14 @@ public class breeder {
         }
     }
 
-    public Boolean isLearnable(skill x, engimon y) {
-        List<String> elements = x.getElements();
-        for (String e : elements) {
-            if (e.equals(y.getElmt1()) || e.equals(y.getElmt2())) {
-                return true;
-            }
-        }
-        return false;
-    }
+    // public Boolean isLearnable(skill x, engimon y) {
+    //     List<String> elements = x.getElements();
+    //     for (String e : elements) {
+    //         if (e.equals(y.getElmt1())) return true;
+    //         if (y.getElmt2() != null) {
+    //             if (e.equals(y.getElmt2())) return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 }
