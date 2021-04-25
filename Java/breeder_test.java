da5cb90f7c9bb3aa6565048ea20dfe79a56ee1d3
Karina Imani <imani.karina@gmail.com>
2021-04-26 02:29:43 +0700

    Minor fixes on breeder

--- Java/breeder_test.java
+++ Java/breeder_test.java
@@ -5,23 +5,23 @@ public class breeder_test {
         try {
             engimon daddad = new wild_engimon();
             engimon mommom = new wild_engimon();
-            engimon dad = new player_engimon("Dad", daddad, mommom, "Lapras", 32, 100);
-            engimon mom = new player_engimon("Mom", daddad, mommom, "Wooper", 33, 200);
+            player_engimon dad = new player_engimon("Dad", daddad, mommom, "Vulpichu", 4, 100);
+            player_engimon mom = new player_engimon("Mom", daddad, mommom, "Charmander", 5, 200);
             // Water, Ice, Water x Ice, Water x Ground
 
             dad.learnMove("tackle");
-            dad.learnMove("bubble");
-            dad.learnMove("ice_beam");
+            //dad.learnMove("bubble");
+            //dad.learnMove("ice_beam");
             mom.learnMove("tackle");
-            mom.learnMove("bubble");
-            mom.learnMove("rock_smash");
+            //mom.learnMove("bubble");
+            //mom.learnMove("rock_smash");
 
             // dad.showAttributes();
             // System.out.println();
             // mom.showAttributes();
             // System.out.println();
 
-            engimon childe = breeder.breed("Childe", dad, mom);
+            player_engimon childe = breeder.breed("Childe", dad, mom);
             System.out.println();
             for (int i = 0; i < 4; i++) {
                 if (childe.getMove(i) != null) {
